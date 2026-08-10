dc157e7d58bb23780a41e7ea0cac1746a0211a1d
Sergey Denisov <sergdenisov@ozon.ru>
2026-08-10 17:03:33 +0300

    f

--- app/components/SwiperForInfoPage/SwiperForInfoPage.tsx
+++ app/components/SwiperForInfoPage/SwiperForInfoPage.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { FreeMode, Navigation, Thumbs } from "swiper/modules";
 import { Swiper, SwiperSlide, type SwiperClass } from "swiper/react";
 
-import "./SwiperStyleForInfoPage.css";
+import "./swiperStyleForInfoPage.css";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
